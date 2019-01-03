849382c786499ceeb2000132eaf3cc8d946459c6
lprice8 <carterprice2@gmail.com>
2019-01-03 16:22:32 -0500

    added terrain with texture, space ship, skybox, waypoint circuit --- far too big of a commit

---
+++ Assets/Splash_delay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Splash_delay : MonoBehaviour {
+
+    // Use this for initialization
+    //private void Awake()
+    //{
+    //    DontDestroyOnLoad(GameObject);
+    //}
+
+    void Start ()
+    {
+        Invoke("LoadFirstScene", 2f);
+	}
+	
+	// Update is called once per frame
+	void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+	}
+}
