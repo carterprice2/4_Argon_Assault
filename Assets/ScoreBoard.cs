3eade5b7fb28ee58bf0b564ef19ddefd4fb934d4
lprice8 <carterprice2@gmail.com>
2019-02-12 19:11:26 -0500

    added sore update and serialized field "ScorePerHit" to enemy

--- Assets/ScoreBoard.cs
+++ Assets/ScoreBoard.cs
@@ -3,22 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreBoard : MonoBehaviour {
+public class ScoreBoard : MonoBehaviour
+{
 
-    [SerializeField] int scorePerHit = 12;
+
 
     int score;
     Text scoreText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         scoreText = GetComponent<Text>();
         scoreText.text = score.ToString();
-	}
-	
+    }
+
     //this is public so any script/game object can call this method
-	public void ScoreHit()
+    public void ScoreHit(int scorePerHit)
     {
         score = score + scorePerHit;
+        scoreText.text = score.ToString();
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,9 @@ public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;  
+
+    ScoreBoard scoreBoard;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +20,15 @@ public class Enemy : MonoBehaviour {
 
     private void AddNonTriggerBoxCollider()
     {
+        addBoxCollder();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+
+    }
+
+    private void addBoxCollder()
+    {
         Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
         enemyCollider.isTrigger = false;
-        
     }
 
     private void OnParticleCollision(GameObject other)
@@ -27,6 +36,7 @@ public class Enemy : MonoBehaviour {
         GameObject FX = Instantiate(deathFX, transform.position, Quaternion.identity);
         FX.transform.parent = parent;
         Destroy(gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
     }
    
 }
